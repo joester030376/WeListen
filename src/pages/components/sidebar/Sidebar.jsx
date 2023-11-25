0e88d4f2ffd0ddb4380d13784e405b3d42a27185
joester030376 <firehawkdigitalohio@gmail.com>
2023-11-24 21:26:49 -0500

    Added more to the sidebar

--- src/pages/components/sidebar/Sidebar.jsx
+++ src/pages/components/sidebar/Sidebar.jsx
@@ -1,155 +1,99 @@
-import * as React from 'react';
-import { useState } from 'react';
-import PropTypes from 'prop-types';
-import Divider from '@mui/material/Divider';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import CssBaseline from '@mui/material/CssBaseline';
-import Drawer from '@mui/material/Drawer';
-import IconButton from '@mui/material/IconButton';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import MenuIcon from '@mui/icons-material/Menu';
-import Toolbar from '@mui/material/Toolbar';
-import List from '@mui/material/List';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import Collapse from '@mui/material/Collapse';
-import DraftsIcon from '@mui/icons-material/Drafts';
-import SendIcon from '@mui/icons-material/Send';
-import ExpandLess from '@mui/icons-material/ExpandLess';
-import ExpandMore from '@mui/icons-material/ExpandMore';
-import StarBorder from '@mui/icons-material/StarBorder';
+import React from 'react'
 import Avatar from '@mui/material/Avatar';
 import Assets from '../../../assests';
-import { Typography } from '@mui/material';
+import DashboardIcon from '@mui/icons-material/Dashboard';
+import PersonOutlineIcon from '@mui/icons-material/PersonOutline';
+import StoreIcon from '@mui/icons-material/Store';
+import CreditCardIcon from '@mui/icons-material/CreditCard';
+import LocalShippingIcon from '@mui/icons-material/LocalShipping';
+import AssessmentIcon from '@mui/icons-material/Assessment';
+import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
+import SettingsSystemDaydreamOutlinedIcon from '@mui/icons-material/SettingsSystemDaydreamOutlined';
+import ArticleOutlinedIcon from '@mui/icons-material/ArticleOutlined';
+import SettingsOutlinedIcon from '@mui/icons-material/SettingsOutlined';
+import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
+import ExitToAppOutlinedIcon from '@mui/icons-material/ExitToAppOutlined';
+import './sidebar.scss'
 
-
-const drawerWidth = 240;
-
-const Sidebar = (props) => {
-  const { window } = props;
-  const [mobileOpen, setMobileOpen] = useState(false);
-  const [open, setOpen] = useState(true);  
-
-  const handleDrawerToggle = () => {
-    setMobileOpen(!mobileOpen);
-  };
-
-  const handleClick = () => {
-    setOpen(!open);
-  };
-
-
-
-  const drawer = (
-    <div>
-      <Toolbar>
-       <Avatar 
-            src={Assets.images.logo} 
-            style={{ borderRadius: 5, justifyContent: "center"}} 
-            sx={{
-                width: 54,
-                height: 54
-            }}
-       /> 
-       <Typography>
-            Firehawk Digital
-       </Typography>                
-      </Toolbar>   
-      <Divider />       
-     <Typography>
-        List Header
-     </Typography>
-     <List
-      sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}
-      component="nav"
-      aria-labelledby="nested-list-subheader"      
-    >
-      <ListItemButton>
-        <ListItemIcon>
-          <SendIcon />
-        </ListItemIcon>
-        <ListItemText primary="Sent mail" />
-      </ListItemButton>
-      <ListItemButton>
-        <ListItemIcon>
-          <DraftsIcon />
-        </ListItemIcon>
-        <ListItemText primary="Drafts" />
-      </ListItemButton>
-      <Typography>
-        Application
-      </Typography>
-      <ListItemButton onClick={handleClick}>
-        <ListItemIcon>
-          <InboxIcon />
-        </ListItemIcon>      
-        <ListItemText primary="Inbox" />
-        {open ? <ExpandLess /> : <ExpandMore />}
-      </ListItemButton>  
-      <Typography>
-        Administration      
-      </Typography>    
-    </List>
-    </div>
-  );  
-
+const Sidebar = () => {
   return (
-    <Box sx={{ display: 'flex' }}>        
-      <CssBaseline />
-      <AppBar
-        position="fixed"
-        sx={{
-          width: { sm: `calc(100% - ${drawerWidth}px)` },
-          ml: { sm: `${drawerWidth}px` },
-        }}
-      >
-        <Toolbar>
-          <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            edge="start"
-            onClick={handleDrawerToggle}
-            sx={{ mr: 2, display: { sm: 'none' } }}
-          >
-            <MenuIcon />
-          </IconButton>          
-        </Toolbar>
-      </AppBar>
-      <Box
-        component="nav"
-        sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
-        aria-label="mailbox folders"
-      >
-        {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
-        <Drawer
-          variant="temporary"
-          open={mobileOpen}
-          onClose={handleDrawerToggle}
-          ModalProps={{
-            keepMounted: true, // Better open performance on mobile.
-          }}
-          sx={{
-            display: { xs: 'block', sm: 'none' },
-            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
-          }}
-        >
-          {drawer}
-        </Drawer>
-        <Drawer
-          variant="permanent"
-          sx={{
-            display: { xs: 'none', sm: 'block' },
-            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
-          }}
-          open
-        >
-          {drawer}
-        </Drawer>
-      </Box>      
-    </Box>
-  );
+    <div className="sidebar">
+        <div className="top">
+            <Avatar 
+                alt="Firehawk Digital" 
+                src={Assets.images.logo} 
+                sx={{
+                    height: 54,
+                    width: 54,
+                    borderRadius: 5
+                }}                
+            />
+            <div className='logo'>Firehawk Digital</div>   
+        </div>
+        <hr />   
+        <div className="center">
+            <ul>
+                <p className="title">MAIN</p>
+                <li>
+                    <DashboardIcon className='icon'/>
+                    <span>Dashboard</span>
+                </li>
+                <p className="title">LISTS</p>
+                <li>
+                    <PersonOutlineIcon className='icon' />
+                    <span>Users</span>
+                </li>
+                <li>
+                    <StoreIcon className='icon'/>
+                    <span>Products</span>
+                </li>
+                <li>
+                    <CreditCardIcon className='icon' />
+                    <span>Orders</span>
+                </li> 
+                <li>
+                    <LocalShippingIcon className='icon' />
+                    <span>Delivery</span>
+                </li>  
+                <p className="title">USEFUL</p>
+                <li>
+                    <AssessmentIcon className='icon' />
+                    <span>Stats</span>
+                </li> 
+                <li>
+                    <NotificationsNoneIcon className='icon'/>
+                    <span>Notifications</span>
+                </li> 
+                <p className="title">SERVICE</p>
+                <li>                
+                    <SettingsSystemDaydreamOutlinedIcon className='icon'/>
+                    <span>System Health</span>
+                </li> 
+                <li>
+                    <ArticleOutlinedIcon className='icon'/>
+                    <span>Logs</span>
+                </li> 
+                <li>
+                    <SettingsOutlinedIcon className='icon'/>
+                    <span>Settings</span>
+                </li> 
+                <p className="title">USER</p>
+                <li>
+                    <AccountCircleOutlinedIcon className='icon'/>
+                    <span>Profile</span>
+                </li> 
+                <li>
+                    <ExitToAppOutlinedIcon className='icon'/>
+                    <span>Logout</span>
+                </li> 
+            </ul>
+        </div>
+        <div className="bottom">
+            <div className="colorOption"></div>
+            <div className="colorOption"></div>            
+        </div>
+    </div>
+  )
 }
 
 export default Sidebar;
